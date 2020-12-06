93272fa0f11f3fe304080f36153fa66a87e99d00
Joseph Cheng <59868375+Reconcube2@users.noreply.github.com>
2020-12-06 16:42:52 +0800

    Add files via upload

---
+++ day6.py
@@ -0,0 +1,41 @@
+import csv
+
+with open('day6_input.csv', newline='') as f:
+    reader = csv.reader(f)
+    data = list()
+    temp_list = list()
+    for line in reader:
+        if line:
+            line = line[0].split(' ')
+            for word in line:
+                temp_list.append(word)
+        else:
+            data.append(temp_list)
+            temp_list = list()
+    data.append(temp_list)
+
+# print(data[:10])
+
+def part_1(data):
+    total = 0
+    for line in data:
+        pool = set()
+        for entry in line:
+            for c in entry:
+                pool.add(c)
+        total += len(pool)
+    return total
+
+def part_2(data):
+    total = 0
+    for line in data:
+        for i in range(len(line)):
+            line[i] = set(line[i])
+        line = set.intersection(*line)
+        total += len(line)
+    return total
+
+ans_1 = part_1(data)
+ans_2 = part_2(data)
+print(f'Part 1: {ans_1}')
+print(f'Part 2: {ans_2}')
