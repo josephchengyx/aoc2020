177dc6070fe929ec366e1df6a3074f6d7b7f4ae1
Joseph Cheng <59868375+Reconcube2@users.noreply.github.com>
2020-12-13 18:47:47 +0800

    Add files via upload

--- day13/day13.py
+++ day13/day13.py
@@ -44,9 +44,11 @@ def part_1(buses, t0):
 def gcdExtended(a, b):
     if a == 0:
         return b, 0, 1
+    
     gcd, x1, y1 = gcdExtended(b%a, a)
     x = y1 - (b//a) * x1
     y = x1
+
     return gcd, x, y
 
 def part_2(buses):
