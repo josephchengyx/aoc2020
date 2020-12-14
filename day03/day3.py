6a3a7d274f4068e1790dd7c08a672bd59c836c9d
Joseph Cheng <59868375+Reconcube2@users.noreply.github.com>
2020-12-14 21:45:40 +0800

    Add files via upload

--- day03/day3.py
+++ day03/day3.py
@@ -1,14 +1,12 @@
-import csv
-
-with open('day3_input.csv', newline='') as f:
-    reader = csv.reader(f)
+with open('day3_input.txt', newline='') as f:
+    reader = f.read().splitlines()
     data = list()
     for line in reader:
-        data.append(line[0])
+        data.append(line)
 
 # print(data[:10])
 
-def part_1(data, rt, dn):
+def count_trees(data, rt, dn):
     trees = 0
     x, y = 0, 0
     m, n = len(data), len(data[0])
@@ -21,8 +19,8 @@ def part_1(data, rt, dn):
             x -= n
     return trees
 
-ans_1 = part_1(data, 3, 1)
-a21, a22, a23, a24 = part_1(data, 1, 1), part_1(data, 5, 1), part_1(data, 7, 1), part_1(data, 1, 2)
+ans_1 = count_trees(data, 3, 1)
+a21, a22, a23, a24 = count_trees(data, 1, 1), count_trees(data, 5, 1), count_trees(data, 7, 1), count_trees(data, 1, 2)
 ans_2 = ans_1 * a21 * a22 * a23 * a24
 print(f'Part 1: {ans_1}')
 print(f'Part 2: {ans_2}')
