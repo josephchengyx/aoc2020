72bd874f3a30f01d8e8a4fb967be2687b85b4a7d
Joseph Cheng <59868375+Reconcube2@users.noreply.github.com>
2020-12-14 21:46:13 +0800

    Add files via upload

--- day05/day5.py
+++ day05/day5.py
@@ -1,10 +1,8 @@
-import csv
-
-with open('day5_input.csv', newline='') as f:
-    reader = csv.reader(f)
+with open('day5_input.txt', newline='') as f:
+    reader = f.read().splitlines()
     data = list()
     for line in reader:
-        data.append(line[0])
+        data.append(line)
 
 # print(data[:10])
 
