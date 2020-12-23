9c861debaf2a1e03841a65187e50fd30ed709ff9
Joseph Cheng <59868375+Reconcube2@users.noreply.github.com>
2020-12-24 00:41:51 +0800

    Add files via upload

--- day23/day23.py
+++ day23/day23.py
@@ -1,10 +1,12 @@
 data = 247819356
 
 def parse(data):
-    return list(map(int, list(str(data))))
+    return tuple(map(int, list(str(data))))
 
 data = parse(data)
 # print(data)
+
+
 
 def parse_v2(data):
     N = len(data)
@@ -26,42 +28,43 @@ def unparse(data):
         res.append(n)
     return ''.join(map(str, res))
 
-
-
 def move_cups(data, ini, moves):
-    def nxt(i, n):
-        if n < 2:
-            return data[i-1]
-        else:
-            return data[nxt(i, n-1)-1]
     minn, maxx = min(data), max(data)
     curr = ini
     for move in range(moves):
-        up = (nxt(curr, 1), nxt(curr, 2), nxt(curr, 3))
-        curr_nxt = nxt(curr, 4)
+        up1 = data[curr-1]
+        up2 = data[up1-1]
+        up3 = data[up2-1]
+        curr_nxt = data[up3-1]
         dest = curr - 1
-        while dest in up or dest < minn:
+        while (dest == up1 or dest == up2 or dest == up3) or dest < minn:
             dest -= 1
             if dest < minn:
                 dest = maxx
-        dest_nxt = nxt(dest, 1)
-        data[dest-1] = up[0]
-        data[up[-1]-1] = dest_nxt
+        dest_nxt = data[dest-1]
+        data[dest-1] = up1
+        data[up3-1] = dest_nxt
         data[curr-1] = curr_nxt
         curr = curr_nxt
     return data
 
 
 
-part_1_data, ini = parse_v2(data)
-part_1_res = move_cups(part_1_data, ini, 100)
-ans_1 = unparse(part_1_res)[1:]
+def part_1(data):
+    data, ini = parse_v2(data)
+    res = move_cups(data, ini, 100)
+    return unparse(res)[1:]
 
-part_2_data, ini = parse_v2(data + list(range(10, 1_000_001)))
-part_2_res = move_cups(part_2_data, ini, 10_000_000)
-a21 = part_2_res[0]
-a22 = part_2_res[a21-1]
-ans_2 = a21 * a22
+def part_2(data):
+    data, ini = parse_v2(data + tuple(range(10, 1_000_001)))
+    res = move_cups(data, ini, 10_000_000)
+    r1 = res[0]
+    r2 = res[r1-1]
+    return r1 * r2
 
+
+
+ans_1 = part_1(data)
+ans_2 = part_2(data)
 print(f'Part 1: {ans_1}')
 print(f'Part 2: {ans_2}')
